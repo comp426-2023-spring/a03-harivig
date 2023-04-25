173863b0f58c3ed38c66a1feb4736796c6d6aae9
harivig <54375773+harivig@users.noreply.github.com>
2023-04-25 12:25:30 -0400

    fixing help messages

--- bin/rps-cli.js
+++ bin/rps-cli.js
@@ -2,7 +2,38 @@
 
 
 import { rps } from "../lib/rpsls.js"
+import minimist from "minimist";
 
 rps()
 
-console.log("hi");
+var args = minimist(process.argv.slice(2));
+
+if (args.h || args.help) {
+    console.log(`Usage: node-rps [SHOT]
+    Play Rock Paper Scissors (RPS)
+    
+      -h, --help      display this help message and exit
+      -r, --rules     display the rules and exit
+    
+    Examples:
+      node-rps        Return JSON with single player RPS result.
+                      e.g. {"player":"rock"}
+      node-rps rock   Return JSON with results for RPS played against a simulated opponent.
+                      e.g {"player":"rock","opponent":"scissors","result":"win"}`);
+    process.exit(0);
+}
+
+if (args.r || args.rules) {
+    console.log(`Rules for Rock Paper Scissors:
+    - Scissors CUTS Paper
+    - Paper COVERS Rock
+    - Rock CRUSHES Scissors`);
+    process.exit(0);
+}
+
+var results = rps(args._[0]);
+
+console.log(JSON.stringify(results));
+process.exit(0)
+
+//  console.log("hi");
